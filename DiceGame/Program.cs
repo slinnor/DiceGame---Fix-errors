c96362a3c1f1bb34b4e636cda68df73566ef1f58
Slinnor <info@danielklement.cz>
2023-09-28 11:26:17 +0200

    Final

--- DiceGame/Program.cs
+++ DiceGame/Program.cs
@@ -5,21 +5,21 @@ double dice2 = random.Next(1, 6);
 
 if (dice1 == dice2)
 {
-    Console.WriteLine($"Endian! Dice 1: {dice1}, Dice 2: {dice2}");
+    Console.Write($"Endian! Dice 1: {dice1}, Dice 2: {dice2}");
 }
 else
-    Console.WriteLine($"You throw dice1 + dice2 points!");
+    Console.Write($"You throw dice1 + dice2 points!");
     if (dice1 + dice2 > 9)
     {
-        ConsoleWriteLine("You throw a high numbers!!!");
-        ConsoleWriteLine($"Dice 1: (dice), Dice 2: {dice2} => {dice1 + dice2}");
+        ConsoleLine("You throw a high numbers!!!");
+        ConsoleLine($"Dice 1: (dice), Dice 2: {dice2} => {dice1 + dice2}");
     }
     else (dice1 + dice2 > 4)
     {
-        Console.WriteLine("You throw a medium numbers!");
+        Console.Write("You throw a medium numbers!");
         Console.WriteLine($"Dice 1: {dice1}, Dice 2: {dice2} => {dice1 + dice2}");
     }
     else
-        Console.WriteLine("You throw a low numbers!")
+        Console.Write("You throw a low numbers!")
         Console.WriteLine($"Dice 1: {dice1}, Dice 2: {dice2} => {dice1 + dice2}");
 }
